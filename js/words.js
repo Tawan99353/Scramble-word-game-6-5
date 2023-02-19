b1956c94ce1bf557e0e93c49895ec2e70842f7f0
Tawan99353 <61555153+Tawan99353@users.noreply.github.com>
2023-02-19 20:24:03 +0700

    Add files via upload

---
+++ js/words.js
@@ -0,0 +1,79 @@
+let words = [
+    { 
+        word: 'apple',
+        hint : 'A fruit with a red or yellow skin and a white interior.' 
+    },
+    { 
+        word: 'banana', 
+        hint: 'A long, curved fruit with a yellow skin and a soft, sweet interior.' 
+        ,
+    },
+    {
+        word: 'cherry', 
+        hint: 'A small, round fruit with a red or black skin and a hard, stony pit.' 
+    },
+    { 
+        word: 'date', 
+        hint: 'A sweet, brown fruit that grows on palm trees.' 
+    },
+    { 
+        word: 'elderberry', 
+        hint: 'A small, dark purple fruit that grows on a shrub.' 
+    },
+    { 
+        word: 'fig', 
+        hint: 'A sweet fruit with a soft, chewy texture and small, crunchy seeds.' 
+    },
+    { 
+        word: 'grape', 
+        hint: 'A small, round fruit with a thin skin and a juicy interior.' 
+    },
+    { 
+        word: 'honeydew', 
+        hint: 'A type of melon with a smooth, pale green skin and a sweet, juicy interior.'
+    },
+    { 
+        word: 'kiwi', 
+        hint: 'A small, oval fruit with a fuzzy brown exterior and a bright green interior.' 
+    },
+    { 
+        word: 'lemon', 
+        hint: 'A sour, yellow fruit with a thick, bumpy skin.' 
+    },
+    { 
+        word: 'mango', 
+        hint: 'A sweet, juicy fruit with a soft, fibrous interior.' 
+    },
+    { 
+        word: 'orange', 
+        hint: 'A round, orange fruit with a thick, bumpy skin and a juicy interior.' 
+    },
+    { 
+        word: 'pear', 
+        hint: 'A sweet fruit with a soft, juicy interior and a smooth, brownish-green skin.' 
+    },
+    { 
+        word: 'quince', 
+        hint: 'A hard, yellow fruit with a fragrant aroma and a sour, astringent taste.' 
+    },
+    { 
+        word: 'raspberry', 
+        hint: 'A small, red or black fruit with a soft, juicy texture and tiny, edible seeds.' 
+    },
+    { 
+        word: 'strawberry', 
+        hint: 'A small, red fruit with a sweet, juicy interior and tiny, edible seeds on the exterior.' 
+    },
+    { 
+        word: 'tangerine', 
+        hint: 'A small, orange fruit with a thin skin and a sweet, juicy interior.' 
+    },
+    { 
+        word: 'watermelon', 
+        hint: 'A large, green melon with a sweet, juicy interior.' 
+    },
+    { 
+        word: 'pineapple', 
+        hint: 'A tropical fruit with a tough, spiky exterior and sweet, juicy interior.' 
+    }
+]
